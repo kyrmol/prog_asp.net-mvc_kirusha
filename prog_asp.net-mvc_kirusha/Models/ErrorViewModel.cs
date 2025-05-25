a990d87b648b316995bf5dfcf8deaf1db083d7cf
Kyrylo <kyrmol@outlook.com>
2025-05-25 22:11:57 +0200

    Add project files.

---
+++ prog_asp.net-mvc_kirusha/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace prog_asp.net_mvc_kirusha.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
